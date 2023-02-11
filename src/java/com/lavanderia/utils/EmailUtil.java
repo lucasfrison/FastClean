19aca28c424f2ec05f6cc399bedd49fdc8808455
lucas <lucfg15@gmail.com>
2023-02-11 17:21:59 -0300

    enviar email OK

---
+++ src/java/com/lavanderia/utils/EmailUtil.java
@@ -0,0 +1,49 @@
+package com.lavanderia.utils;
+
+import java.util.Date;
+
+import jakarta.mail.Message;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+public class EmailUtil {
+
+	/**
+	 * Utility method to send simple HTML email
+	 * @param session
+	 * @param toEmail
+	 * @param subject
+	 * @param body
+	 */
+	public static void sendEmail(Session session, String toEmail, String subject, String body){
+		try
+	    {
+	      MimeMessage msg = new MimeMessage(session);
+	      //set message headers
+	      msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+	      msg.addHeader("format", "flowed");
+	      msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+	      msg.setFrom(new InternetAddress("fastclean156@gmail.com", "FastClean"));
+
+	      msg.setReplyTo(InternetAddress.parse("fastclean156@gmail.com", false));
+
+	      msg.setSubject(subject, "UTF-8");
+
+	      msg.setText(body, "UTF-8");
+
+	      msg.setSentDate(new Date());
+
+	      msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+	      System.out.println("Message is ready");
+              Transport.send(msg);  
+
+	      System.out.println("EMail Sent Successfully!!");
+	    }
+	    catch (Exception e) {
+	      e.printStackTrace();
+	    }
+	}
+}
